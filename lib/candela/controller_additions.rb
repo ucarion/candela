2e65838800d69c0ad2420a63d09105b80321c5ef
Ulysse Carion <ulyssecarion@gmail.com>
2013-10-09 15:49:45 -0700

    Add support for #new and #edit actions for authorize_resource.

--- lib/candela/controller_additions.rb
+++ lib/candela/controller_additions.rb
@@ -2,13 +2,18 @@ module Candela
   module ControllerAdditions
     def self.authorize_resource(controller_class, opts)
       controller_name = controller_class.controller_name
-      instance_name = controller_name.singularize
+      instance_name = controller_name.singularize 
 
-      controller_class.prepend_before_filter only: :show do |controller|
+      controller_class.prepend_before_filter do |controller|
         params = controller.params
         model = params[:controller].classify.constantize
-        
-        controller.instance_variable_set("@#{instance_name}", model.find(params[:id]))
+
+        case params[:action].to_sym
+        when :new
+          controller.instance_variable_set("@#{instance_name}", model.new)
+        when :show, :edit
+          controller.instance_variable_set("@#{instance_name}", model.find(params[:id]))
+        end
       end
     end
   end
