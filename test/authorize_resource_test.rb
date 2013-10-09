e3848e164c24aba23f56d9fa148d7d770fb58cbf
Ulysse Carion <ulyssecarion@gmail.com>
2013-10-09 15:48:24 -0700

    Test for properly handling #edit and #new actions for non-nested resources.

--- test/authorize_resource_test.rb
+++ test/authorize_resource_test.rb
@@ -12,4 +12,16 @@ class AuthorizeResourceTest < ActionController::TestCase
 
     assert_not_nil assigns(:user)
   end
+
+  test "loading from params[:id] on edit" do
+    get(:edit, id: 1)
+
+    assert_not_nil assigns(:user)
+  end
+
+  test "creating a new model on new" do
+    get(:new)
+
+    assert_not_nil assigns(:user)
+  end
 end
