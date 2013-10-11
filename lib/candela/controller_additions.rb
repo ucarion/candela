5beda976467f4114090cebe3b1408f3e24048386
Ulysse Carion <ulyssecarion@gmail.com>
2013-10-11 02:21:44 -0700

    Refactor: move authorize_resource into its own class and make it legible.

--- lib/candela/controller_additions.rb
+++ lib/candela/controller_additions.rb
@@ -1,33 +1,92 @@
 module Candela
-  module ControllerAdditions
-    def self.authorize_resource(controller_class, opts)
-      controller_name = controller_class.controller_name
-      instance_name = controller_name.singularize
-
-      controller_class.prepend_before_filter do |controller|
-        model = params[:controller].classify.constantize
-
-        target = case params[:action].to_sym
-        when :new
-          if opts[:parent]
-            parent_class = opts[:parent].to_s.classify.constantize
-            parent_id = params["#{opts[:parent]}_id"]
-
-            instance = parent_class.find(parent_id).send(controller_name).new
-
-            instance_variable_set("@#{instance_name}", instance)
-          else
-            instance_variable_set("@#{instance_name}", model.new)
-          end
-        when :create
-          created_instance = model.new(send(opts[:params]))
-          instance_variable_set("@#{instance_name}", created_instance)
-        else
-          instance_variable_set("@#{instance_name}", model.find(params[:id]))
-        end
-
-        raise AccessDeniedError unless current_ability.can?(params[:action].to_sym, target)
+  class ResourceAuthorizer
+    # Though this may look weird, it's actually a pretty effective way
+    # at creating a class that adds functionality to a controller without
+    # polluting its namespace. In all other methods, `self` will refer to
+    # a ResourceAuthorizer, not any controller.
+    def self.add_before_filter(controller_class, method_name, opts)
+      controller_class.before_filter do |controller|
+        ResourceAuthorizer.new(controller, opts).send(method_name)
       end
+    end
+
+    def initialize(controller, opts)
+      @controller = controller
+      @params = @controller.params
+      @opts = opts
+    end
+
+    # TODO: handle #update properly.
+    def authorize_resource
+      loaded_resource = case action
+      when :new
+        load_resource_for_action_new
+      when :create
+        load_resource_for_action_create
+      else
+        load_resource_from_id
+      end
+
+      check_can_access_resource(loaded_resource)
+    end
+
+    private
+
+    def check_can_access_resource(resource)
+      unless @controller.current_ability.can?(action, resource)
+        raise AccessDeniedError
+      end
+    end
+
+    def action
+      @params[:action].to_sym
+    end
+
+    def load_resource_for_action_new
+      if @opts[:parent]
+        child = parent_model.find(parent_id).send(plural_model_name).new
+        set_resource_instance_var(child)
+      else
+        set_resource_instance_var(model_class.new)
+      end
+    end
+
+    def load_resource_for_action_create
+      set_resource_instance_var(model_class.create(strong_params))
+    end
+
+    def load_resource_from_id
+      set_resource_instance_var(model_class.find(@params[:id]))
+    end
+
+    def set_resource_instance_var(value)
+      ivar_name = "@#{singular_model_name}"
+      @controller.instance_variable_set(ivar_name, value)
+    end
+
+    def parent_model
+      @opts[:parent].to_s.camelize.constantize
+    end
+
+    def parent_id
+      @params[@opts[:parent].to_s.foreign_key]
+    end
+
+    # TODO: A better name for this?
+    def strong_params
+      @controller.send(@opts[:params])
+    end
+
+    def model_class
+      singular_model_name.classify.constantize
+    end
+
+    def singular_model_name
+      plural_model_name.singularize
+    end
+
+    def plural_model_name
+      @controller.controller_name
     end
   end
 end
--- lib/candela/controller_extensions.rb
+++ lib/candela/controller_extensions.rb
@@ -3,8 +3,44 @@ module Candela
     extend ActiveSupport::Concern
 
     module ClassMethods
+      # Creates a before_filter which will load a resource into an instance
+      # variable, then make sure that loaded variable can be accessed.
+      # 
+      # The process of loading the resource is quite straightforward. Consider
+      # this usage of the authorize_resource method:
+      # 
+      #   class ArticlesController < ApplicationController
+      #     authorize_resource, params: :article_params
+      #   end
+      # 
+      # Then, the @article variable will be populated as:
+      # 
+      #   #new          => Article.new
+      #   #create       => Article.create(article_params)
+      #   anything else => Article.find(params[:id])
+      # 
+      # If your resource is in fact a nested one, then use the `parent` parameter
+      # when calling authorize_resouce, passing the singular name of the parent 
+      # class as the value of `parent`:
+      # 
+      #   class CommentsController < ApplicationController
+      #     authorize_resource, params: :article_params, parent: :article
+      #   end
+      # 
+      # Resource loading will remain largely unchanged, but loading for the #new
+      # action will be modified:
+      # 
+      #   #new => Article.find(params[:article_id]).comments.new
+      # 
+      # Options:
+      # [:+params+]
+      #   The method to call on the controller to create / update a model.
+      # [:+parent+]
+      #   If this model is a nested one, then set this value to be the singular name
+      #   of the parent model.
+      # 
       def authorize_resource(opts = {})
-        ControllerAdditions.authorize_resource(self, opts)
+        ResourceAuthorizer.add_before_filter(self, :authorize_resource, opts)
       end
     end
 
